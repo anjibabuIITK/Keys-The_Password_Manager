83e79791b82dd67815baa0470918cfa167291ee3
KAPAKAYALA ANJI BABU <anjibabu480@gmail.com>
2021-01-20 16:17:31 +0530

    updated reset_masterkey

--- src/reset_masterkey.sh
+++ src/reset_masterkey.sh
@@ -2,9 +2,7 @@
 # This code is a part of Keys tool
 #---------------------------------------------#
 #!/bin/bash
-#Check .Password_Manager/etc/profile exists or not. if not create
 path=.Password_Manager/etc/profile
-file=.Password_Manager/etc/profile/profile
 master_file=.Password_Manager/etc/profile/masterkey
 [ -d $path ] ||mkdir -p $path
 #---------------------------------------------#
@@ -16,57 +14,38 @@ master_key=`cat $master_file |awk '{print $1}'`
 #echo "$master_key"
 }
 #---------------------------------------------#
-function set_profile() {
-echo;echo " User Registration: "
-read -p "Name : " usr_name
-read -p "Email: " usr_mail
-read -p "Phone Number:" phone_nmbr
-
-#decrypt the file
-cat > $file <<EOF
- $usr_name:$usr_mail:$phone_nmbr
-EOF
-#encrypt 
-
-
-read -p " Wanna reset Master Key [yes/no]: " option
-echo "$option"
+function reset_master_key() {
+read -p "  Are you sure to reset Master Key [yes/no]: " option
+#echo "$option"
 if [[ "$option" == "yes" ]];then
-echo " Resetting Master Key: "
-read -p "Enter Master key:" master_key
+echo "   Resetting Master Key: "
+echo "   Enter New Master key: "
+read -s  master_key
 #decrypt the file
 cat > $master_file <<EOF
 $master_key
 EOF
 #encrypt 
 else
-echo " Master Key has not changed."
+echo "   Master Key has not updated."
 fi
-
 
 #bash src/encrypt.sh -en $path/profile
 }
 #---------------------------------------------#
-#  main
+#   Main code
 #---------------------------------------------#
-#[ -f  "$file"  ] && bash src/encrypt.sh -en $file
-#if $file is not empty, print user profile already existed, use reset profile 
-# Access this code by asking master key, use default master key as 123456
-# use reset_master key command to update masterkey. there ask previous masterkey to update.
 get_master_key
 #Ask user to enter masterkey
-echo "Enter Master Key: "
+echo ;echo "  Enter Present Master Key: "
 read -s user_entered_key
 
 #[ $user_entered_key -eq $master_key ] && echo "Password matched." || echo "Entered wrong password." 
 if [[ "$user_entered_key" == "$master_key" ]];then
-   echo "Access Granted." 
-
-# Ask user to set profile
-set_profile
-echo;echo " User has been registered successfully. "
-
+   echo "   Access Granted." 
+   reset_master_key
+echo "   Master Key has been updated successfully."
 else
-   echo "Entered wrong password." 
+   echo "   Entered wrong password." 
    exit
 fi
